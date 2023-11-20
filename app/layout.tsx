4bd99a03ae17d4cd3fd4b139edc2be98d24bf2b2
Reza <reza.ngr8@gmail.com>
2023-11-20 02:51:23 -0800

    change font to open sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Team Chat Application',
