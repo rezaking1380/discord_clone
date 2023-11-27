3dba49e898779dabb7bcf3432bc58ea5d76fddf7
Reza <reza.ngr8@gmail.com>
2023-11-27 10:13:29 -0800

    add model store , modal provider , server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -31,6 +32,7 @@ export default function RootLayout({
           storageKey="discord-theme"
           disableTransitionOnChange
           >
+          <ModalProvider />
           {children}
           </ThemeProvider>
           </body>
